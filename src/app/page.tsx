905700f1aefaa4e1ea9acca9af2a542b7d37d8a4
Eric Tsai <eric492718@gmail.com>
2024-08-05 00:04:19 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,57 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col items-center gap-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="72"
+        height="72"
+        viewBox="0 0 250 250"
+        fill="#151513"
+        className="group"
+      >
+        <a
+          title="View Source"
+          href={
+            "https://github.com/vercel/virtual-event-starter-kit/blob/main/components/view-source.tsx"
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <path d="M0 0l115 115h15l12 27 108 108V0z" fill="#fff" />
+          <path
+            className="group-hover:animate-wave origin-custom"
+            d="M128 109c-15-9-9-19-9-19 3-7 2-11 2-11-1-7 3-2 3-2 4 5 2 11 2 11-3 10 5 15 9 16"
+          />
+          <path d="M115 115s4 2 5 0l14-14c3-2 6-3 8-3-8-11-15-24 2-41 5-5 10-7 16-7 1-2 3-7 12-11 0 0 5 3 7 16 4 2 8 5 12 9s7 8 9 12c14 3 17 7 17 7-4 8-9 11-11 11 0 6-2 11-7 16-16 16-30 10-41 2 0 3-1 7-5 11l-12 11c-1 1 1 5 1 5z" />
+        </a>
+      </svg>
+
+      <div className="w-full max-w-96 min-w-60 h-14">
+        <span className="block relative h-full w-full rounded-lg overflow-hidden bg-slate-200 dark:bg-slate-600">
+          <span
+            className="block absolute top-0 left-0 w-full h-full bg-orange-400 animate-pulse"
+            style={{ left: `-${100 - 80}%` }}
+          />
+          <span className="block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-xs font-bold text-center leading-4">
+            Uploading... (80)%
+          </span>
+        </span>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="w-24 h-24 bg-slate-500 hover:cursor-pointer hover:animate-scale-up"></div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="w-24 h-24 bg-slate-500 hover:cursor-pointer hover:animate-scale-down"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <div className="w-12 h-12 bg-slate-500 hover:cursor-pointer hover:animate-spin"></div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <div className="w-12 h-12 bg-slate-500 hover:cursor-pointer hover:animate-wiggle"></div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className="w-12 h-12 bg-slate-500 hover:cursor-pointer hover:animate-shake"></div>
+
+      <div className="w-12 h-12 bg-slate-500 hover:cursor-pointer hover:animate-shift-left"></div>
+
+      <div className="w-12 h-12 bg-slate-500 hover:cursor-pointer hover:animate-fade-out"></div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,67 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        wave: {
+          "0%,  100%": {
+            transform: "rotate(0deg)",
+          },
+          "20%": {
+            transform: "rotate(-25deg)",
+          },
+          "40%": {
+            transform: "rotate(10deg)",
+          },
+          "60%": {
+            transform: "rotate(-25deg)",
+          },
+          "80%": {
+            transform: "rotate(10deg)",
+          },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+        pulse: {
+          "50%": {
+            opacity: ".8",
+          },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        shake: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-2px)" },
+          "50%": { transform: "translateX(0px)" },
+          "75%": { transform: "translateX(2px)" },
+        },
+        shiftLeft: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100px)" }, // Adjust -100px to how far left you want it to go
+        },
+        "scale-up": {
+          from: { transform: "scale(0.6)" },
+          to: { transform: "scale(1)" },
+        },
+        "scale-down": {
+          from: { transform: "scale(1)" },
+          to: { transform: "scale(0)" },
+        },
+      },
+      animation: {
+        wave: "wave 560ms ease-in-out",
+        wiggle: "wiggle 1s ease-in-out infinite", // animate-[wiggle_1s_ease-in-out_infinite]
+        shake: "shake 0.3s linear infinite",
+        "shift-left": "shiftLeft 0.5s ease-in-out forwards",
+        "scale-up": "scale-up 0.5s ease-in-out forwards",
+        "scale-down": "scale-down 0.5s ease-in-out forwards",
+        "fade-out": "fade-out 1s ease-in-out",
+      },
+      transformOrigin: {
+        custom: "130px 106px",
       },
     },
   },
